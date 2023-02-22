33bed821d00ebadad7074d7271ea1f928d9cc95e
Oluwatimilehin Esan <107879861+therealtmmy@users.noreply.github.com>
2023-02-22 03:01:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let errorEl = document.getElementById("error")
-console.log(errorEl)
+let hamBurger = document.getElementById("icon")
+let overlay = document.querySelector(".navs")
+let layTwo = document.getElementById("list")
+
+hamBurger.addEventListener("click", ()=> {
+   hamBurger.classList.toggle('icon');
+    overlay.classList.toggle("overlay");
+    layTwo.classList.toggle("overlay");
+});
 
 const Rest = () => {
     errorEl.innerText = "Please Check Again"
