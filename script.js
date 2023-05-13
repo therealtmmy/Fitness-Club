e161e75ba05ec52d4a065c69b4f9d92e2a42a69a
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-05-13 15:47:22 +0100

    alert

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-let errorEl = document.getElementById("error")
-let hamBurger = document.getElementById("icon")
-let overlay = document.querySelector(".navs")
-let layTwo = document.getElementById("list")
+alert(
+  "Welcome To Fitness-Club, This project would be completed soon. Thank you for glasing through."
+);
 
-hamBurger.addEventListener("click", ()=> {
-   hamBurger.classList.toggle('icon');
-    overlay.classList.toggle("overlay");
-    layTwo.classList.toggle("overlay");
+let errorEl = document.getElementById("error");
+let hamBurger = document.getElementById("icon");
+let overlay = document.querySelector(".navs");
+let layTwo = document.getElementById("list");
+
+hamBurger.addEventListener("click", () => {
+  hamBurger.classList.toggle("icon");
+  overlay.classList.toggle("overlay");
+  layTwo.classList.toggle("overlay");
 });
 
 const Rest = () => {
-    errorEl.innerText = "Please Check Again"
-}
+  errorEl.innerText = "Please Check Again";
+};
